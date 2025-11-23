eed5cd61fe4ef41699d3aaa6e53b8640b438595e
Philippe DeSousa <phil.desousa@gmail.com>
2025-11-22 23:20:20 -0500

    update cors and auth

--- constants.ts
+++ constants.ts
@@ -7,7 +7,9 @@ export const TableName = 'AppnameApples';
 
 export const headers = {
     'Access-Control-Allow-Origin': OriginURL,
-    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Headers':
+        'Content-Type, x-amz-content-sha256, x-amz-date, X-Amz-Security-Token, Authorization',
+    'Access-Control-Allow-Credentials': 'true',
 };
 
 export const InvalidItemError = 'invalid item id';
